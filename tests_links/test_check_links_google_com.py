e2901839fc27cc4c761d806a21196b1899efee92
Aleksey Kokorev <a.kokorev@timeweb.ru>
2024-07-27 00:37:23 +0800

    added test

--- tests_links/test_check_links_google_com.py
+++ tests_links/test_check_links_google_com.py
@@ -1,30 +1,34 @@
 import pytest
 import allure
 
-from tools.link_checker import link_checker
+from tools.link_checker.cheker import LinkCheckerFactory
+
+link_checker = LinkCheckerFactory.create_checker('async')
 
 
 @allure.epic('Проверка всех ссылок на странице с использованием selene')
+@pytest.mark.asyncio
 @pytest.mark.parametrize("page_url", [
     "https://www.google.com",
-
+    "https://example.com"
 ])
-def test_check_all_links_with_selene(page_url):
+async def test_check_all_links_with_selene(page_url):
     """
     Тест для проверки всех ссылок на странице с использованием selene.
     """
-    broken_links = link_checker.check_links_on_page_with_selene(page_url)
+    broken_links = await link_checker.check_links_on_page_with_selene(page_url)
     link_checker.assert_no_broken_links(broken_links)
 
 
 @allure.epic('Проверка всех ссылок на странице с использованием BeautifulSoup')
+@pytest.mark.asyncio
 @pytest.mark.parametrize("page_url", [
     "https://www.google.com",
-
+    "https://example.com"
 ])
-def test_check_all_links_with_bs4(page_url):
+async def test_check_all_links_with_bs4(page_url):
     """
     Тест для проверки всех ссылок на странице с использованием BeautifulSoup.
     """
-    broken_links = link_checker.check_links_on_page_with_bs4(page_url)
+    broken_links = await link_checker.check_links_on_page_with_bs4(page_url)
     link_checker.assert_no_broken_links(broken_links)
