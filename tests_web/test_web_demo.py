e44f140e3f3be290af89e2f7eee794b509fa251a
Aleksey Kokorev <a.kokorev@timeweb.ru>
2024-09-04 15:54:28 +0800

    Добавлен тест для демонстрации упавших шагов

--- tests_web/test_web_demo.py
+++ tests_web/test_web_demo.py
@@ -1,17 +1,17 @@
-import allure
 import pytest
 
 from tools.web import web
 
 
 @pytest.mark.regression
-@allure.severity(allure.severity_level.NORMAL)
-@allure.title('Basic scenario')
-@allure.description('Basic scenario')
-@allure.testcase('', '')
-@allure.link('', '')
-@allure.issue('', '')
 def test_web_demo():
     web.login_page.open()
     web.login_page.fill_auth_form()
+    web.login_page.press_login_button()
+
+
+@pytest.mark.regression
+def test_web_demo_fail():
+    web.login_page.open()
+    web.login_page.fill_auth_form(password='wrong_password')
     web.login_page.press_login_button()
